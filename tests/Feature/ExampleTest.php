e0dc52c24f3a8746911acf1c1243a6c2a41ab38c
Matias Echazarreta <echazarreta.matias@gmail.com>
2021-03-24 00:09:31 -0300

    Added test

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -2,20 +2,18 @@
 
 namespace Tests\Feature;
 
+use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class ExampleTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     *
-     * @return void
-     */
+    use DatabaseMigrations;
+
     public function test_example()
     {
-        $response = $this->get('/');
+        $response = $this->get(route('products.index'));
 
         $response->assertStatus(200);
     }
